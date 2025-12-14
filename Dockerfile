bb64ab1f3484f3e0a79c3b43f91cf08598dac425
Nurislam <nurislam.triniti@gmail.com>
2025-12-14 20:01:48 +0500

    Major update

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM python:3.11-slim
 
+RUN echo "deb http://ftp.ru.debian.org/debian trixie main contrib non-free" > /etc/apt/sources.list && \
+    echo "deb http://ftp.ru.debian.org/debian-security trixie-security main contrib non-free" >> /etc/apt/sources.list
+
 RUN apt-get update && apt-get install -y npm && \
     npm install -g configurable-http-proxy && \
     apt-get clean && rm -rf /var/lib/apt/lists/*
@@ -8,11 +11,14 @@ RUN useradd -m -s /bin/bash jupyter
 
 RUN pip install --no-cache-dir jupyterhub jupyterlab
 
-RUN mkdir -p /home/jupyter/notebooks && \
-    chown -R jupyter:jupyter /home/jupyter
+RUN mkdir -p /home/jupyter/notebooks /home/jupyter/data /srv/jupyterhub && \
+    chown -R jupyter:jupyter /home/jupyter /srv/jupyterhub
 
 COPY jupyterhub_config.py /srv/jupyterhub/jupyterhub_config.py
+RUN chown jupyter:jupyter /srv/jupyterhub/jupyterhub_config.py
 
 EXPOSE 8000
+
+USER jupyter
 
 CMD ["jupyterhub", "-f", "/srv/jupyterhub/jupyterhub_config.py"]
--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -1,13 +1,30 @@
+import os
 c = get_config()
-
+data_dir = os.environ.get('JUPYTERHUB_DATA_DIR', '/home/jupyter/data')
+# ----------------------------
+# Authenticator
+# ----------------------------
 c.JupyterHub.authenticator_class = 'dummy'
 c.DummyAuthenticator.password = "admin"
 
+# ----------------------------
+# Spawner
+# ----------------------------
 c.JupyterHub.spawner_class = 'localprocess'
-
 c.LocalProcessSpawner.user = 'jupyter'
 c.LocalProcessSpawner.notebook_dir = '/home/jupyter/notebooks'
-
 c.Spawner.cmd = ['jupyter-labhub']
 
+# ----------------------------
+# Bind URL
+# ----------------------------
 c.JupyterHub.bind_url = 'http://:8000'
+
+# ----------------------------
+# Cookie secret
+# ----------------------------
+c.JupyterHub.cookie_secret_file = os.path.join(data_dir, "jupyterhub_cookie_secret")
+c.JupyterHub.db_url = f"sqlite:///{os.path.join(data_dir, 'jupyterhub.sqlite')}"
+
+
+c.ConfigurableHTTPProxy.pid_file = os.path.join(data_dir, "configurable-http-proxy.pid")
